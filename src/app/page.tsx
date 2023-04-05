f576e9388690b41b232270fcf88b8421ecc47726
Kevin Mulcrone <KayBeSee@gmail.com>
2023-04-04 23:09:13 -0600

    update front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ const OptionItem = ({ option }: { option: Option }) => (
   <li className="text-slate-900 flex items-center justify-center hover:bg-gray-100/50 hover:ring-gray-500/50 hover:ring-1 rounded-2xl transition-all duration-200">
     <Link
       href={option.href}
-      className="flex items-center justify-center h-full w-full py-4 px-5 text-lg font-semibold"
+      className="flex items-center justify-center h-full w-full py-2 px-3 sm:py-4 sm:px-5 text-lg font-semibold"
     >
       {option.title}
     </Link>
@@ -35,7 +35,7 @@ const OptionItem = ({ option }: { option: Option }) => (
 export default function Home() {
   return (
     <main className="h-screen w-screen p-24 flex flex-col items-center justify-center from-white via-[#F3EFEB] to-white bg-gradient-to-br">
-      <div className="pt-0 pb-0">
+      <div className="pt-0 pb-4 sm:pb-0">
         <div className="h-64 w-64 relative">
           <Image
             src="/ROUNDTABLE.svg"
@@ -46,7 +46,7 @@ export default function Home() {
         </div>
       </div>
       <div className="">
-        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-1">
           {options.map((option) => (
             <OptionItem key={option.href} option={option} />
           ))}
